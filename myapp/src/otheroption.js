a1ae71373697491a1209639dc911f5436e2a0f07
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2021-12-26 15:05:28 -0300

    c22-adding new change about otherop

---
+++ myapp/src/otheroption.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Otheropt = (props) =>{
+    return(
+    
+  
+      <option>
+      {props.earn}
+      </option>
+      
+    
+    )
+  }
+
+export default Otheropt  
